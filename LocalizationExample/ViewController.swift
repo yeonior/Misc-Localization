cf56323cd19d98f241603b2151c65a187009de1f
Rus <de.mr44@gmail.com>
2021-10-21 16:22:18 +0500

    Added a localization that set programmatically

--- LocalizationExample/ViewController.swift
+++ LocalizationExample/ViewController.swift
@@ -9,12 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var textView: UITextView!
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        textView.text = NSLocalizedString("text_example", comment: "")
     }
 }
 
